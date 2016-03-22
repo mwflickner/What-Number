b38af385d8b406d4a4b0df1c0c47cb31d784e407
Matthew <mwflickner@gmail.com>
2016-03-22 15:15:20 -0700

    more bizing

--- js/changeToBiz.js
+++ js/changeToBiz.js
@@ -1,22 +1,35 @@
-//var elements = document.getElementsByTagName('*');
-var $elements = $('*').not("script, style, link");
+(function () {
 
-for (var i = 0; i < $elements.length; i++) {
-    var element = $elements[i];
+    var biz = function(){
+        var $elements = $('*').not("script, style, link");
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+        for (var i = 0; i < $elements.length; i++) {
+            var element = $elements[i];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var bizifiedText = text.replace(/5/g, "Biz")
-                                .replace(/Five/g, "Biz")
-                                .replace(/five/g, "biz")
-                                .replace(/FIVE/g, "BIZ");
+            for (var j = 0; j < element.childNodes.length; j++) {
+                var node = element.childNodes[j];
 
-            if (bizifiedText !== text) {
-                element.replaceChild(document.createTextNode(bizifiedText), node);
+                if (node.nodeType === 3) {
+                    var text = node.nodeValue;
+                    var bizifiedText = text.replace(/5/g, "Biz")
+                                        .replace(/Five/g, "Biz")
+                                        .replace(/five/g, "biz")
+                                        .replace(/FIVE/g, "BIZ");
+
+                    if (bizifiedText !== text) {
+                        element.replaceChild(document.createTextNode(bizifiedText), node);
+                    }
+                }
             }
         }
     }
-}
+
+    $(document).ready(function(){
+        biz();
+    });
+
+    $(window).load(function(){
+        biz();
+    });
+        
+})();
